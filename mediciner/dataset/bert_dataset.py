e817d446e8d29b82c146e18e248ad766bcbaaca5
nccuSimonLee <simon2020291@gmail.com>
2020-11-02 13:32:58 +0000

    [add] add `adjust_labels_by_encoding()` to bert_dataset.py

--- mediciner/dataset/bert_dataset.py
+++ mediciner/dataset/bert_dataset.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from tokenizers import Encoding
 from typing import Dict, List, Tuple
 import os
 import pandas as pd
@@ -19,6 +20,21 @@ def read_corpus(path_to_corpus_dir: str) -> Dict[int, List[str]]:
 def read_ents_table(path_to_ents_table: str) -> pd.DataFrame:
     ents_table = pd.read_csv(path_to_ents_table)
     return ents_table
+
+def adjust_labels_by_encoding(encoding: Encoding, labels: List[int], subword_label: int) -> List[int]:
+    adjusted_labels = []
+    for token, (start, end) in zip(encoding.tokens, encoding.offsets):
+        if _is_special_token(token):
+            continue
+        label = subword_label if _is_subword(token) else labels[start]
+        adjusted_labels.append(label)
+    return adjusted_labels
+
+def _is_special_token(token: str) -> bool:
+    return token in ('[CLS]', '[SEP]')
+
+def _is_subword(token: str) -> bool:
+    return token[:2] == '##'
 
 
 class BertDataset(torch.utils.data.Dataset):
