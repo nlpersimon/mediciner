16684ac870b7f7dbe8f7d31c9a1c622730aaa08b
nccuSimonLee <simon2020291@gmail.com>
2020-11-02 06:26:03 +0000

    [add] implement `label_corpus()` and `tag_corpus()` of `CorpusLabeler`

--- mediciner/dataset/corpus_labeler.py
+++ mediciner/dataset/corpus_labeler.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 class CorpusLabeler(object):
     def __init__(self, entity_types: List[str]) -> None:
@@ -17,3 +17,17 @@ class CorpusLabeler(object):
             label_begin = label_inner + 1
             label_inner = label_begin + 1
         return tag_to_label
+    
+    def label_corpus(self, corpus: str, entity_spans: List[Tuple[int, int, str]]) -> List[int]:
+        corpus_tags = self.tag_corpus(corpus, entity_spans)
+        corpus_labels = [self.tag_to_label[tag] for tag in corpus_tags]
+        return corpus_labels
+    
+    def tag_corpus(self, corpus: str, entity_spans: List[Tuple[int, int, str]]) -> List[str]:
+        corpus_tags = ['O'] * len(corpus)
+        for begin, end, ent_type in entity_spans:
+            tag_begin, tag_inner = f'B-{ent_type}', f'I-{ent_type}'
+            assert tag_begin in self.tag_to_label and tag_inner in self.tag_to_label
+            corpus_tags[begin] = tag_begin
+            corpus_tags[(begin + 1):end] = [tag_inner] * (end - begin - 1)
+        return corpus_tags
