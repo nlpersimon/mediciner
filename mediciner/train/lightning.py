0a5b3f2c2d34ae888848b6901c1de7c4c3300173
nccuSimonLee <simon2020291@gmail.com>
2020-11-03 03:02:56 +0000

    [add] implement `training_step()` of `BertLightning`

--- mediciner/train/lightning.py
+++ mediciner/train/lightning.py
@@ -4,9 +4,10 @@ from transformers import BertForTokenClassification
 
 
 class BertLightning(pl.LightningModule):
-    def __init__(self, bert_model: BertForTokenClassification):
+    def __init__(self, bert_model: BertForTokenClassification, use_logger: bool = False) -> None:
         super().__init__()
         self.bert_model = bert_model
+        self.use_logger = False
 
     def forward(self, x):
         input_ids, attention_mask = x
@@ -14,7 +15,11 @@ class BertLightning(pl.LightningModule):
         return outputs
 
     def training_step(self, batch, batch_idx):
-        raise NotImplementedError
+        input_ids, attention_mask, labels = batch
+        outputs = self.bert_model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs['loss']
+        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=self.use_logger)
+        return loss
 
     def configure_optimizers(self):
         raise NotImplementedError
