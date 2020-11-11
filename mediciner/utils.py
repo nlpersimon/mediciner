e52483065b0f83bb3846ddc902f7f854fc7eceb1
nccuSimonLee <simon2020291@gmail.com>
2020-11-11 15:03:16 +0000

    [add] add `build_ents_table()` to mediciner/utils.py

--- mediciner/utils.py
+++ mediciner/utils.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
+import pandas as pd
 from tokenizers import BertWordPieceTokenizer
-from typing import List
+import tqdm
+from typing import List, Dict
+from .extractor.bert_extractor import BertExtractor, Entity
+from .dataset.processor import BertProcessor, Example
 
 
 
@@ -32,3 +36,28 @@ def sentences_to_paragraphs(sentences: List[str],
     if hold_parag:
         paragraphs.append(Paragraph(parag_start, parag_end, hold_parag))
     return paragraphs
+
+def build_ents_table(corpus: Dict[int, List[str]],
+                     processor: BertProcessor,
+                     bert_extractor: BertExtractor,
+                     batch_size: int=32) -> pd.DataFrame:
+    examples = processor.convert_corpus_to_examples(corpus)
+    packed_examples = [examples[i:(i + batch_size)] for i in range(0, len(examples), batch_size)]
+    ents_matrix = []
+    for pack in tqdm.tqdm(packed_examples, desc='extract entities from the corpus'):
+        example_ents = extract_entities(pack, bert_extractor)
+        for example, ents in zip(pack, example_ents):
+            _, article_id = example.id.split('-')
+            ent_rows = [[int(article_id), ent.start, ent.end, ent.text, ent.type]
+                        for ent in ents]
+            ents_matrix.extend(ent_rows)
+    ents_table = pd.DataFrame(ents_matrix,
+                              columns=['article_id', 'start_position', 'end_position', 'entity_text', 'entity_type'])
+    return ents_table
+
+def extract_entities(examples: List[Example], bert_extractor: BertExtractor) -> List[List[Entity]]:
+    example_entities = bert_extractor.extract_entities([example.content for example in examples])
+    entities = [[Entity(ent.start + example.start, ent.end + example.start, ent.text, ent.type)
+                 for ent in ents]
+                for example, ents in zip(examples, example_entities)]
+    return entities
