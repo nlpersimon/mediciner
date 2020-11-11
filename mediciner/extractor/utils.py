7200fc01bb68e1b849285500f158a87b73289bd7
nccuSimonLee <simon2020291@gmail.com>
2020-11-11 14:04:11 +0000

    [refactor] move the auxiliary functions for `BertExtractor` to extractor/utils.py

---
+++ mediciner/extractor/utils.py
@@ -0,0 +1,53 @@
+from tokenizers import Encoding
+import torch
+from typing import List, Tuple
+from ..dataset.corpus_labeler import label_to_tag
+
+
+
+
+def encodings_to_features(encodings: List[Encoding],
+                          padding_id: int,
+                          max_input_len: int) -> Tuple[torch.Tensor, torch.Tensor]:
+    input_ids_list, attention_mask_list = [], []
+    for encoding in encodings:
+        clipped_input_ids = encoding.ids[:max_input_len]
+        clipped_att_mask = encoding.attention_mask[:max_input_len]
+        padding_len = max_input_len - len(clipped_input_ids)
+        padded_input_ids = clipped_input_ids + [padding_id] * padding_len
+        padded_att_mask = clipped_att_mask + [0] * padding_len
+        input_ids_list.append(padded_input_ids)
+        attention_mask_list.append(padded_att_mask)
+    input_ids = torch.tensor(input_ids_list, dtype=torch.long)
+    attention_mask = torch.tensor(attention_mask_list, dtype=torch.float)
+    return (input_ids, attention_mask)
+
+def unpad_labels(labels: torch.Tensor, attention_mask: torch.Tensor) -> List[torch.Tensor]:
+    unpadded_labels = [example_labels[example_att == 1]
+                       for example_labels, example_att in zip(labels, attention_mask)]
+    return unpadded_labels
+
+def translate_labels_to_tags(pred_labels: torch.Tensor,
+                             input_len: int,
+                             token_spans: List[Tuple[int, int]]) -> List[str]:
+    input_tags = ['O'] * input_len
+    for (start, end), label in zip(token_spans, pred_labels):
+        input_tags[start:end] = expand_tag(label_to_tag(int(label)), end - start)
+    return input_tags
+
+def expand_tag(tag: str, n_char: int) -> List[str]:
+    if tag[0] == 'B':
+        _, ent_type = tag.split('-')
+        expanded_tags = [tag] + ([f'I-{ent_type}'] * (n_char - 1))
+    else:
+        expanded_tags = [tag] * n_char
+    return expanded_tags
+
+def adjust_pred_tags(pred_tags: List[str]) -> List[str]:
+    adjusted_tags, prev_type = [], ''
+    for tag in pred_tags:
+        if tag == 'X':
+            tag = f'I-{prev_type}' if prev_type else 'O'
+        prev_type = tag[2:]
+        adjusted_tags.append(tag)
+    return adjusted_tags
