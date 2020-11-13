a41a2a930a8f95f09493beef1251374aa3fdf2cb
nccuSimonLee <simon2020291@gmail.com>
2020-11-13 04:55:02 +0000

    [chore] move some classes and functions to __init__.py of modules

---
+++ mediciner/dataset/__init__.py
@@ -0,0 +1,3 @@
+from .bert_dataset import BertDataset
+from .processor import BertProcessor, Example, Feature
+from .utils import read_corpus, read_ents_table
---
+++ mediciner/extractor/__init__.py
@@ -0,0 +1,1 @@
+from .bert_extractor import Entity, BertExtractor
