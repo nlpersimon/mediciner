4f386b271055ce5fdb9e0f6d91b4c58ca324db85
nccuSimonLee <simon2020291@gmail.com>
2020-12-21 02:24:09 +0000

    update build_entity_table.sh

--- build_entity_table.sh
+++ build_entity_table.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=$PWD:$PYTHONPATH
 
-python bin/build_entity_table.py --path-to-corpus-dir='data/dialogue_lined/dev' \
+python bin/build_entity_table.py --path-to-corpus-dir='data/dialogue_lined/test' \
                                  --path-to-vocab='vocab/bert-base-chinese-vocab.txt' \
-                                 --path-to-model-dir='model/multi-sents-baseline-further-pretrain-linearLR-lr5e-5-epochs120' \
-                                 --path-to-output='multi-sents-baseline-further-pretrain-linearLR-lr5e-5-epochs120.tsv' \
+                                 --path-to-model-dir='model/multi-sents-test-further-pretrain-lr2.25e-5' \
+                                 --path-to-output='multi-sents-test-further-pretrain-lr2.25e-5.tsv' \
                                  --gpu=0 \
                                  --mode='multi-sents' \
                                  --batch-size=8 \
