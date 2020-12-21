724ae5c43a5f2aef7470ce4167d9f1c443a68f7b
nccuSimonLee <simon2020291@gmail.com>
2020-12-21 03:44:40 +0000

    [add] implement `BertWithCRF.forward()`

--- mediciner/ner_model/crf_on_top.py
+++ mediciner/ner_model/crf_on_top.py
@@ -11,3 +11,16 @@ class BertWithCRF(BertPreTrainedModel):
         self.position_wise_ff = nn.Linear(config.hidden_size, config.num_labels)
         self.crf = CRF(num_tags=config.num_labels, batch_first=True)
         self.init_weights()
+    
+    def forward(self, input_ids, attention_mask, labels=None):
+        bert_outputs = self.bert(input_ids=input_ids,
+                                 attention_mask=attention_mask,
+                                 return_dict=True)
+        last_hidden_state = self.dropout(bert_outputs['last_hidden_state'])
+        outputs = {}
+        emissions = self.position_wise_ff(last_hidden_state)
+        if labels is not None:
+            loss = self.crf(emissions, labels, attention_mask.byte(), 'token_mean')
+            outputs['loss'] = -1 * loss
+        outputs['emissions'] = emissions
+        return outputs
