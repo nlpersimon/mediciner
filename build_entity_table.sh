820bb8b599515590f4289c4c8f0d68cc4648dff7
nccuSimonLee <simon2020291@gmail.com>
2020-12-22 00:38:33 +0000

    modify build_entity_table.sh to crf version

--- build_entity_table.sh
+++ build_entity_table.sh
@@ -2,9 +2,10 @@ export PYTHONPATH=$PWD:$PYTHONPATH
 
 python bin/build_entity_table.py --path-to-corpus-dir='data/dialogue_lined/test' \
                                  --path-to-vocab='vocab/bert-base-chinese-vocab.txt' \
-                                 --path-to-model-dir='model/multi-sents-test-further-pretrain-lr2.25e-5' \
-                                 --path-to-output='multi-sents-test-further-pretrain-lr2.25e-5.tsv' \
+                                 --path-to-model-dir='model/multi-sents-test-further-pretrain-crf-ep50' \
+                                 --path-to-output='test-multi-sents-test-further-pretrain-crf-ep50.tsv' \
                                  --gpu=0 \
                                  --mode='multi-sents' \
                                  --batch-size=8 \
+                                 --crf \
                                  --max-input-len=510
