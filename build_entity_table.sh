44feb5f02c3fa8e85854745da1c5196e0cb318c8
nccuSimonLee <simon2020291@gmail.com>
2020-12-26 12:34:01 +0000

    update scripts

--- build_entity_table.sh
+++ build_entity_table.sh
@@ -1,11 +1,10 @@
 export PYTHONPATH=$PWD:$PYTHONPATH
 
 python bin/build_entity_table.py --path-to-corpus-dir='data/dialogue_lined/test' \
-                                 --path-to-vocab='vocab/bert-base-chinese-vocab.txt' \
-                                 --path-to-model-dir='model/multi-sents-test-further-pretrain-crf-ep50' \
-                                 --path-to-output='test-multi-sents-test-further-pretrain-crf-ep50.tsv' \
-                                 --gpu=0 \
+                                 --path-to-vocab='vocab/bert-base-multilingual-cased-vocab.txt' \
+                                 --path-to-model-dir='model/multi-sents-multiling-lr2.25e-5' \
+                                 --path-to-output='test-multi-sents-multiling-lr2.25e-5.tsv' \
+                                 --gpu=1 \
                                  --mode='multi-sents' \
                                  --batch-size=8 \
-                                 --crf \
                                  --max-input-len=510
